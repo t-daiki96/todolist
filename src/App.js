d3ea8dd13eede555fc57003c16ff0e3e8ac5d5a5
t-daiki96 <t.daiki96@gmail.com>
2020-02-24 21:35:22 +0900

    create delete function

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import Form from './Form';
 import './css/app.css';
 
 export default () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([])
+  let [id, setId] = useState([])
 
   const addTodo = todo => {
-    setTodos([...todos, { id: todos.length + 1, title: todo.title, desc: todo.desc, done: false }])
+    setTodos([...todos, { id, title: todo.title, desc: todo.desc, done: false }])
+    setId(++id)
   }
 
   const toggleDone = id => {
     const i = todos.findIndex(todo => todo.id === id)
     const newTodos = todos.concat()
     newTodos[i].done = !todos[i].done
+    setTodos(newTodos)
+  }
+
+  const deleteToDo = id => {
+    const newTodos = todos.filter(todo => todo.id !== id)
     setTodos(newTodos)
   }
 
@@ -26,6 +33,7 @@ export default () => {
       <ToDoList
         todos={todos}
         toggleDone={toggleDone}
+        deleteToDo={deleteToDo}
       />
     </div>
   );
--- src/ToDo.js
+++ src/ToDo.js
@@ -3,19 +3,25 @@ import React from 'react'
 import './css/todo.css'
 
 export default props => {
-  const status = props.done ? '済' : '未完了'
+  const status = props.done ? 'return' : 'done'
   const className = props.done ? 'done' : 'undone'
 
   return (
-    <li class={className}>
-      <span class='title'>{props.title}</span>
+    <li className={className}>
+      <span className='title'>{props.title}</span><br />
+      <p>{props.desc}</p>
       <span
         onClick={() => props.toggleDone(props.id)}
-        class='status'
+        className='status'
       >
         {status}
       </span>
-      <p>{props.desc}</p>
+      <span
+        onClick={() => props.deleteToDo(props.id)}
+        className='delete'
+      >
+        delete
+      </span>
     </li>
   )
 }
